34ace9b8e510be720e1903e5ec8d23a39130d236
Dana <mitsunokehl@gmail.com>
2024-06-28 16:15:39 +0300

    Updates

---
+++ assets/DashboardPage-CvX62Jkq.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-Dsfupc1B.js";const s=()=>a.jsx("div",{children:"DashboardPage"});export{s as default};
---
+++ assets/RegistrationPage-Cv5DUtvL.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Dsfupc1B.js";const r=()=>t.jsx("div",{children:"RegistrationPage"});export{r as default};
